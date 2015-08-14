2536a55db0737f93db2bd7c8bbe3fa6b8cfe6552
1hemant <demonvsgod@gmail.com>
2015-08-14 13:29:57 +0530

    Trying to add city pointers

---
+++ JSON/Anand.js
@@ -0,0 +1,23 @@
+{
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "id": "01",
+            "name": "Nahapa",
+            "coordinates": [
+                72.91490256786346,
+                22.476535393799885
+            ],
+            "text": "Welcome to Nahapa"
+        },
+        {
+            "id": "02",
+            "name": "Khambhat",
+            "coordinates": [
+                72.62429594993591,
+                22.31550151087173
+            ],
+            "text": "Welcome to Khambhat"
+        }
+    ]
+}
