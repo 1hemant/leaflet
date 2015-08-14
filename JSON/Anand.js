c6895332fd8fbde21f1b1ab95e501c7d4633a7be
1hemant <demonvsgod@gmail.com>
2015-08-14 18:52:32 +0530

    Added markers for city.
    Not using anand.js

--- JSON/Anand.js
+++ JSON/Anand.js
@@ -1,23 +1,27 @@
-{
+
+var districtname = {
     "type": "FeatureCollection",
     "features": [
         {
-            "id": "01",
-            "name": "Nahapa",
-            "coordinates": [
-                72.91490256786346,
-                22.476535393799885
-            ],
-            "text": "Welcome to Nahapa"
-        },
-        {
-            "id": "02",
-            "name": "Khambhat",
-            "coordinates": [
-                72.62429594993591,
-                22.31550151087173
-            ],
-            "text": "Welcome to Khambhat"
+            "type": "Feature",
+            "properties": {
+                "Name": "Nahapa"
+                "Text": "Welcome to Nahapa"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [72.91490256786346, 22.476535393799885]
+            }
+        },{
+            "type": "Feature",
+            "properties": {
+                "Name": "Khambhat"
+                "Text": "Welcome to Khambhat"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [72.62429594993591, 22.31550151087173]
+            }
         }
     ]
-}
+};
