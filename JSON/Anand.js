af045ed958bbd773ab8666ac2700676c0b265034
1hemant <demonvsgod@gmail.com>
2015-08-18 18:59:36 +0530

    City is loading and showing data.
    City are loading and showing data.

--- JSON/Anand.js
+++ JSON/Anand.js
@@ -1,27 +1,72 @@
-
-var districtname = {
-    "type": "FeatureCollection",
-    "features": [
+//var littleton = L.marker([22.47, 72.91]).bindPopup('Nahapa.'),
+//    denver    = L.marker([22.31, 72.62]).bindPopup('This is Denver, CO.'),
+//var cities = L.layerGroup([littleton, denver]);
+var AnandData = [{
+    "type": "Feature",
+    "properties": {
+        "name": "Nahapa",
+        "popupContent": "This is Nahapa!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [22.47, 72.91]
+    }
+},
         {
-            "type": "Feature",
-            "properties": {
-                "Name": "Nahapa"
-                "Text": "Welcome to Nahapa"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [72.91490256786346, 22.476535393799885]
-            }
-        },{
-            "type": "Feature",
-            "properties": {
-                "Name": "Khambhat"
-                "Text": "Welcome to Khambhat"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [72.62429594993591, 22.31550151087173]
-            }
-        }
-    ]
-};
+    "type": "Feature",
+    "properties": {
+        "name": "Bharauch",
+        "popupContent": "Bharauch!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [22.31, 72.62]
+    }
+}];
+        
+        
+        
+//        {
+//    "type": "Feature",
+//    "features": [
+//        {
+//            "type": "Feature",
+//            "id": "01",
+//            "properties": {
+//                "name": "Nahapa",
+//                "density": 94.65
+//            },
+//            "geometry": {
+//                "type": "Point",
+//                "coordinates": [
+//                    [
+//                        [
+//                            22.476535393799885,
+//                            72.91490256786346
+//                            
+//                        ]
+//                    ]
+//                ]
+//            }
+//        },
+//        {
+//            "type": "Feature",
+//            "id": "01",
+//            "properties": {
+//                "name": "Nahapa",
+//                "density": 94.65
+//            },
+//            "geometry": {
+//                "type": "Point",
+//                "coordinates": [
+//                    [
+//                        [
+//                            22.31550151087173,
+//                            72.62429594993591
+//                        ]
+//                    ]
+//                ]
+//            }
+//        }
+//    ]
+//};
